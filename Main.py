858277b4868d478a7e2a75bf8999c28b570088cf
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2023-12-11 18:47:36 -0600

    First commit

---
+++ Main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    # some code
+    print("Bot Run Succesfully")
---
+++ ServerManger.py
@@ -0,0 +1,7 @@
+import python_aternos
+from python_aternos.python_aternos import Client
+
+
+# methods for the bot to run
+def InitializeClient(UserName, Password):
+    client = Client()
